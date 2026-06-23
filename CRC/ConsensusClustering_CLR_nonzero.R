c8323a035f995c7aa2248fc9def14f2ac4ab417e
anisssum <112714231+anisssum@users.noreply.github.com>
2026-06-23 18:50:24 +0700

    Create ConsensusClustering_CLR_nonzero.R

---
+++ CRC/ConsensusClustering_CLR_nonzero.R
@@ -0,0 +1,30 @@
+#!/usr/bin/Rscript
+
+library(ConsensusClustering)
+library(compositions)
+library(zCompositions)
+
+data <- read.csv('celltype_sample_counts_ImmuneFilter_compact.csv', stringsAsFactors=FALSE, head = TRUE, row.names = 1, check.names = FALSE)
+prop <- sweep(data, 2, colSums(data), "/")
+prop <- prop + min(prop[prop>0])/2
+clr_mat <- clr(t(prop))
+
+Adj <- adj_mat(clr_mat,
+               method="euclidean")
+
+CM = consensus_matrix(Adj,
+                      max.cluster = 6, 
+                      resample.ratio = 0.7, 
+                      max.itter = 500, 
+                      clustering.method = 'pam')
+
+Result <- cc_cluster_count(CM, plot.cdf = TRUE)
+LogitScore = Result[["LogitScore"]]
+PAC = Result[["PAC"]]
+deltaA = Result[["deltaA"]]
+CMavg = Result[["CMavg"]]
+
+Kopt = Result[["Kopt_LogitScore"]]
+message(paste0("The optimum number of clusters = ", Kopt))
+
+save.image(file='consensus_matrix_CRC_ImmuneFilter_compact_clr_temp3.RData')
